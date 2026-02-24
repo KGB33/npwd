2be91cdcdaf9fdf8381844ef176b47233bb42fd0
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2026-02-23 20:00:28 -0800

    Generate GHA yaml via Nix

--- flake.nix
+++ flake.nix
@@ -5,11 +5,15 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     devenv.url = "github:cachix/devenv";
+    github-actions-nix.url = "github:synapdeck/github-actions-nix";
   };
 
   outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;} {
-      imports = [inputs.devenv.flakeModule];
+      imports = [
+        inputs.devenv.flakeModule
+        inputs.github-actions-nix.flakeModule
+      ];
       systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
       perSystem = {
         config,
@@ -42,7 +46,53 @@
             };
           };
         };
-        packages.default = pkgs.hello;
+        packages = {
+          workflows = pkgs.writeShellApplication {
+            name = "copy-workflows";
+            text = ''
+              cp -r ${config.githubActions.workflowsDir} ./.github/workflows
+              chmod -R u+w ./.github/workflows
+            '';
+          };
+        };
+        githubActions = {
+          enable = true;
+          workflows = let
+          in {
+            testServer = {
+              name = "Unit test server";
+              on = ["push" "pull_request"];
+              defaults.run.workingDirectory = "./server";
+              jobs = {
+                test = {
+                  runsOn = "ubuntu-latest";
+                  steps = [
+                    {
+                      uses = "actions/checkout@v4";
+                    }
+                    {
+                      uses = "erlef/setup-beam@v1";
+                      with_ = {
+                        otp-version = "28";
+                        gleam-version = "1.14.0";
+                        rebar3-version = "3";
+                      };
+                    }
+                    {
+                      run = "gleam deps download";
+                    }
+                    {
+                      run = "gleam test";
+                    }
+                    {
+                      run = "gleam format --check src test";
+                    }
+                  ];
+                };
+              };
+            };
+          };
+        };
       };
       flake = {};
     };
